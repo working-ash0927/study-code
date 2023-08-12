a48b9f18bbfd6ae3b4523c303c24a36f970e20a8
working-ash0927 <103326981+working-ash0927@users.noreply.github.com>
2023-08-13 02:15:50 +0900

    terraform cloud migration

--- terraform/dev/providor.tf
+++ terraform/dev/providor.tf
@@ -6,15 +6,22 @@ terraform {
       version = ">=5.0, <=5.6.2"
     }
   }
+  cloud {
+    organization = "workingash"
+    hostname = "app.terraform.io" # Optional; defaults to app.terraform.io
+    workspaces {
+      name = "my_tfcloud"
+    }
+  }
   # backend "local" {  # terraform으로 관리되는 리소스의 정보를 저장하는 위치 지정.
   #   path = "state/terraform.tfstate"
   # }
 
-  backend "s3" {
-    bucket         = "9368b2f3d0-tfstate"                    # s3 bucket 이름
-    key            = "terraform/study/dev/terraform.tfstate" # s3 내에서 저장되는 경로를 의미합니다.
-    region         = "ap-northeast-2"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
+  # backend "s3" {
+  #   bucket         = "9368b2f3d0-tfstate"                    # s3 bucket 이름
+  #   key            = "terraform/study/dev/terraform.tfstate" # s3 내에서 저장되는 경로를 의미합니다.
+  #   region         = "ap-northeast-2"
+  #   encrypt        = true
+  #   dynamodb_table = "terraform-lock"
+  # }
 }
